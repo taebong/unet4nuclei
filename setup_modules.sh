492ea1760a167d48349e211e3f10b0ecdc455dff
Tae Yeon Yoo <tyyoo@holylogin04.rc.fas.harvard.edu>
2024-06-01 11:34:38 -0400

    to use the codes in fas rc

--- setup_modules.sh
+++ setup_modules.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-module load gcc/6.2.0
-module load cuda/9.0
-module load python/3.6.0
+#module load gcc/6.2.0
+module load gcc/13.2.0-fasrc01
+#module load cuda/9.0
+module load cuda/12.4.1-fasrc01
+#module load python/3.6.0
+module load python/3.10.13-fasrc01
 
 source nucleiseg_env/bin/activate
 
